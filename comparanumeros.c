2c32b8e3d7ef2800ffa1bd63a2caf51ebb7ec920
catfigueiredo <142894280+catfigueiredo@users.noreply.github.com>
2023-08-29 07:46:40 -0300

    Add files via upload

--- comparanumeros.c
+++ comparanumeros.c
@@ -5,5 +5,13 @@ int main (){
     scanf("%i",&x);
     printf("digite y:\n");
     scanf("%i",&y);
-
-   }
+    if (x>y) {
+        printf("x é maior \n");
+    }else{
+        if (y>x) {
+            printf("y é maior \n");
+        }else{
+            printf ("x e y são iguais \n");
+        }
+    }
+}
