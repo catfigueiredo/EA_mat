4983985cb077bff5bd5a5c70cf1692df0f07d87e
catfigueiredo <142894280+catfigueiredo@users.noreply.github.com>
2023-09-12 08:36:25 -0300

    Add files via upload

---
+++ circunferencia.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ int main ()
+ {
+     float a,r;
+
+     printf("Insira o valor do raio:\n");
+     scanf("%f", &r);
+     a=3.14*r*r;
+     printf("Area da circunferencia= %.2f", a);
+
+ }
