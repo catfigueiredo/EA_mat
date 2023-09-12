5fd846a77503cc05639427d90e677f1f351d94c3
catfigueiredo <142894280+catfigueiredo@users.noreply.github.com>
2023-09-12 08:11:44 -0300

    Add files via upload

---
+++ quadrado_soma.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+ int main()
+ {
+     int a,b,c,d;
+     printf("Insira o valor de A:\n");
+     scanf("%i", &a);
+     printf("Insira o valor de B:\n");
+     scanf("%i", &b);
+     printf("Insira o valor de C:\n");
+     scanf("%i", &c);
+     d=(a+b+c)*(a+b+c);
+     printf("O quadrado da soma dos valores= %i", d);
+
+ }
