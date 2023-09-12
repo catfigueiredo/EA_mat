9bc3a43707f631f282e9930fc2dfe27ad61ac102
catfigueiredo <142894280+catfigueiredo@users.noreply.github.com>
2023-09-12 08:20:42 -0300

    Add files via upload

---
+++ produto13soma24.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+ int main()
+ {
+    int a,b,c,d,s,p;
+
+     printf("Insira o valor de A:\n");
+     scanf("%i", &a);
+     printf("Insira o valor de B:\n");
+     scanf("%i", &b);
+     printf("Insira o valor de C:\n");
+     scanf("%i", &c);
+     printf("Insira o valor de D:\n");
+     scanf("%i", &d);
+     p=a*c;
+     s=b+d;
+     printf("Produto de A e C= %i \n",p);
+     printf("Soma de B e D= %i",s);
+
+
+ }
