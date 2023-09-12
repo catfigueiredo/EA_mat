5bd8791103feaca793f1934278afb6d60f87c22b
catfigueiredo <142894280+catfigueiredo@users.noreply.github.com>
2023-09-12 07:54:44 -0300

    Add files via upload

---
+++ converterDpR.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+  void main()
+  {
+      float cotacao, dolares, conversao;
+      printf("Insira a cotacao atual:\n");
+      scanf("%f", &cotacao);
+      printf("Insira o valor em dolares:\n");
+      scanf("%f", &dolares);
+      conversao=cotacao*dolares;
+      printf("valor em reais= %.2f",conversao);
+
+
+  }
