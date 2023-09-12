7f42566bebc648a830e967d212ba7f705c295dcf
catfigueiredo <142894280+catfigueiredo@users.noreply.github.com>
2023-09-12 07:57:10 -0300

    Add files via upload

---
+++ converterRpD.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+  void main()
+  {
+      float cotacao, reais, conversao;
+      printf("Insira a cotacao atual:\n");
+      scanf("%f", &cotacao);
+      printf("Insira o valor em reais:\n");
+      scanf("%f", &reais);
+      conversao=cotacao*reais;
+      printf("valor em dolares= %.2f",conversao);
+
+  }
+
