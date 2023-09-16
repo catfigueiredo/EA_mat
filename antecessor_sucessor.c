c48b8af54fe4418105bad31b01affbe496c4232b
catfigueiredo <142894280+catfigueiredo@users.noreply.github.com>
2023-09-16 15:47:15 -0300

    Add files via upload

---
+++ antecessor_sucessor.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+  int main ()
+   {
+     int x,S,A;
+
+     printf("Insira um valor inteiro: ");
+     scanf("%i", &x);
+
+     S= x+1;
+     A= x-1;
+
+     printf("Sucessor: %i \n", S);
+     printf("Antecessor: %i", A);
+   }
+
