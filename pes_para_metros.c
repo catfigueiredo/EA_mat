ad93fe0ae26e5e235a52683e2d5852f6f4a1ab58
catfigueiredo <142894280+catfigueiredo@users.noreply.github.com>
2023-09-16 15:32:18 -0300

    Add files via upload

---
+++ pes_para_metros.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+  int main()
+   {
+       float P, M;
+       printf ("Insira o valor em pes: ");
+       scanf ("%f", &P);
+
+       M= P*0.3048;
+
+       printf("Equivalencia em metros: %.3f", M);
+   }
