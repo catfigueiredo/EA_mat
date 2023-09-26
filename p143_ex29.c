7538002cc9487568553b167ffebc2340b16f1806
catfigueiredo <142894280+catfigueiredo@users.noreply.github.com>
2023-09-26 08:04:29 -0300

    Add files via upload

---
+++ p143_ex29.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+ int main ()
+ {
+     int x;
+     printf("Insira a idade do usuario:\n");
+     scanf("%i",&x);
+
+     if(x>=18)
+        {
+        printf("O usuario e maior de idade. \n");
+        }
+    else if(x<18)
+        {
+        printf("O usuario e menor de idade. \n");
+        }
+
+ }
+
