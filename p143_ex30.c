17bab14ce52a9136459f9ef8be18b771f97b6e2c
catfigueiredo <142894280+catfigueiredo@users.noreply.github.com>
2023-09-30 11:35:59 -0300

    Add files via upload

---
+++ p143_ex30.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+ int main()
+   {
+       int n,x;
+       printf("Insira um valor inteiro: \n");
+       scanf("%i",&x);
+       x=n % 2;
+
+       if (x==0)
+        {
+          printf("O valor e par. \n");
+        }
+        else
+        {
+            printf("O valor e impar.\n");
+        }
+
+        }
