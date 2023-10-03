44fe7f5a3d253ebe21c894e188435c5a1dc4a8fd
catfigueiredo <142894280+catfigueiredo@users.noreply.github.com>
2023-10-03 08:08:05 -0300

    Update p143_ex33.c

--- p143_ex33.c
+++ p143_ex33.c
@@ -16,6 +16,10 @@
         {
             printf("Triangulo equilatero.");
         }
+        else if (l1==0&&l2==0&&l3==0)
+        {
+            printf("Triangulo invalido");
+        }
 
         else if (l1==l2||l1==l3||l2==l3)
         {
