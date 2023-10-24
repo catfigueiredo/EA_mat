b8ae72be79bc0dbd1648159b981ff8a5ba49ac43
catfigueiredo <142894280+catfigueiredo@users.noreply.github.com>
2023-10-24 08:27:45 -0300

    Add files via upload

---
+++ p141_d.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c;
+    a=0;
+    b=0;
+    while (a<=500)
+    {
+        b=b+a;
+        a=a+2;
+
+    }
+   printf("A somatorio dos numeros pares de 0 a 500 e igual a: %i",b);
+
+}
